fae9e7e4ad5013e521e99c6e4deff4f975f548e8
Maciej Leszczyński <dev@asista.pl>
2025-03-20 14:19:45 +0100

    fix: viewport height

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { Header } from './Header'
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className='min-h-screen flex flex-col gap-10 pb-10 bg-neutral-50'>
+    <div className='min-h-dvh flex flex-col gap-10 pb-10 bg-neutral-50'>
       <Header />
       <main className='container'>{children}</main>
     </div>
