1d7e27afec2a868b86a276433a39d8537b761386
Maciej Leszczyński <dev@asista.pl>
2025-03-20 14:16:30 +0100

    fix: focus state

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -8,7 +8,7 @@ export const Input: FC<InputProps> = ({ className, ...props }) => {
     <input
       type='text'
       className={cn(
-        'h-9 rounded-md border border-neutral-200 px-3 py-2 text-base sm:text-sm placeholder:text-neutral-400 shadow shadow-neutral-100 outline-sky-500',
+        'h-9 rounded-md border border-neutral-200 px-3 py-2 text-base sm:text-sm placeholder:text-neutral-400 shadow shadow-neutral-100 outline-sky-500 focus:outline-2',
         className
       )}
       {...props}
